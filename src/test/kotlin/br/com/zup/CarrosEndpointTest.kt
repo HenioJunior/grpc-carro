820abf929d38d70fe708f3ddd40aaa26eb9db6be
Henio Junior <henioajunior@gmail.com>
2021-08-25 12:28:01 -0300

    Nao deve adicionar novo carro quando carro com placa ja existente

--- src/test/kotlin/br/com/zup/CarrosEndpointTest.kt
+++ src/test/kotlin/br/com/zup/CarrosEndpointTest.kt
@@ -1,12 +1,15 @@
 package br.com.zup
 
 import io.grpc.ManagedChannel
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
 import io.micronaut.context.annotation.Factory
 import io.micronaut.grpc.annotation.GrpcChannel
 import io.micronaut.grpc.server.GrpcServerChannel
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import javax.inject.Singleton
 
 @MicronautTest(transactional = false)
@@ -20,6 +23,7 @@ internal class CarrosEndpointTest(val repository : CarroRepository, val grpcClie
 
     @Test
     fun `deve adicionar um novo carro`() {
+
         //cenario
         repository.deleteAll()
 
@@ -35,6 +39,28 @@ internal class CarrosEndpointTest(val repository : CarroRepository, val grpcClie
             assertTrue(repository.existsById(id)) //efeito colateral(insert no banco)
         }
 
+    }
+
+    @Test
+    fun `nao deve adicionar novo carro quando carro com placa ja existente`() {
+
+        //cenario
+        repository.deleteAll()
+        val existente = repository.save(Carro(modelo = "Palio", placa = "OIP-9876"))
+
+        //acao
+       val error = assertThrows<StatusRuntimeException> {
+           grpcClient.adicionar(CarroRequest.newBuilder()
+                                           .setModelo("Versa")
+                                           .setPlaca(existente.placa)
+                                           .build())
+       }
+
+        //validacao
+        with(error) {
+            assertEquals(Status.ALREADY_EXISTS.code, status.code)
+            assertEquals("carro com placa existente", status.description)
+}
     }
 
     @Factory
